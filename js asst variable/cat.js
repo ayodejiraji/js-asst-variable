f71cbd0c2b0850a5d7400a1dfa662732bb2c1bce
ayodejiraji <123636562+ayodejiraji@users.noreply.github.com>
2023-04-03 11:01:32 +0100

    Add files via upload

---
+++ js asst variable/cat.js
@@ -0,0 +1,18 @@
+var animal= "cat";
+var animal;
+console.log(animal);
+
+var catpet="bella";
+var catpet;
+console.log(catpet);
+
+// Strings:
+let petname = "bella";
+let bellasound = "hiss";
+console.log(bellasound);
+
+var x="petcat";
+var x;
+console.log(x);
+
+
---
+++ js asst variable/dog.js
@@ -0,0 +1,20 @@
+var animal="dog";
+var animal;
+console.log(animal);
+
+var dogpet="lusy";
+var dogpet;
+console.log(dogpet);
+
+// Strings:
+let petname = "lusy";
+let lusysound = "Barking";
+console.log(lusysound);
+
+var x="petdog";
+var x;
+console.log(x);
+
+var x="petcat";
+var x;
+console.log(x);
